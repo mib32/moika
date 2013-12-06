d8864bf617dab867472e3ff018f6bda74019fe87
Sergey Vatagin <s.vatagin@gmail.com>
2013-12-06 23:55:28 +0400

    add download to presentation link in car_wash owner info page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,14 @@ class StaticPagesController < ApplicationController
   before_action :set_static_page, only: [:show, :edit, :update, :destroy]
 
   def show
+    respond_to do |format|
+      format.html {}
+      format.pptx {
+          send_file("#{Rails.root}/app/presentations/moika-77.pptx",
+                                  filename: "moika-77.pptx",
+                                  type: "application/vnd.openxmlformats-officedocument.presentationml.presentation")
+      }
+    end
   end
 
   private
