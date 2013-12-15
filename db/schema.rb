acfafabd2cbb4d9dbc41209a1b794ccfff900920
Sergey Vatagin <s.vatagin@gmail.com>
2013-12-15 20:31:48 +0400

    add action_on_map

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131206082703) do
+ActiveRecord::Schema.define(version: 20131215155850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20131206082703) do
     t.string   "official_title"
     t.string   "u_address"
     t.string   "inn"
+    t.boolean  "action_on_map",  default: false
   end
 
   create_table "comments", force: true do |t|
