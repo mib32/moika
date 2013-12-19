d8a5e912d2cc7d37e82425298ed6418a2511eb39
Sergey Vatagin <s.vatagin@gmail.com>
2013-12-19 13:50:18 +0400

    add yet hardcoded client youtube to carwash show TODO add to admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131215155850) do
+ActiveRecord::Schema.define(version: 20131219094402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20131215155850) do
     t.string   "u_address"
     t.string   "inn"
     t.boolean  "action_on_map",  default: false
+    t.boolean  "youtubed",       default: false
   end
 
   create_table "comments", force: true do |t|
