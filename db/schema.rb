17d3ffbf5837c65f8f3d8185303478e2ff97485a
Sergey Vatagin <s.vatagin@gmail.com>
2014-01-19 09:34:29 +0400

    add rating:integer to car_wash

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131224171122) do
+ActiveRecord::Schema.define(version: 20140119052742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20131224171122) do
     t.boolean  "youtubed",       default: false
     t.string   "vk_url"
     t.string   "odn_url"
+    t.integer  "rating",         default: 0
   end
 
   create_table "comments", force: true do |t|
