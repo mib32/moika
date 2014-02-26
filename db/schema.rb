f8716152abf1d8d0319b01f702767b81c99f7de1
Sergey Vatagin <s.vatagin@gmail.com>
2014-02-27 01:43:43 +0400

    add tire attr to carwash && signal

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140226205130) do
+ActiveRecord::Schema.define(version: 20140226213645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20140226205130) do
     t.boolean  "brended",        default: false
     t.boolean  "gasolined",      default: false
     t.boolean  "repaired",       default: false
+    t.boolean  "tired",          default: false
   end
 
   create_table "comments", force: true do |t|
