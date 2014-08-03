9d95dc479d7e7b1209e103eae70ff97cd2408d91
Anton Murygin <mibus32@gmail.com>
2014-08-03 16:58:38 +0400

    Changed capistrano origin

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 set :scm, "git"
-set :repository, "git@github.com:vatic/#{application}.git"
+set :repository, "git@github.com:mib32/#{application}.git"
 set :branch, "master"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
